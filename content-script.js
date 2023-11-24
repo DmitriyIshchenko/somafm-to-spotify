9f15b1c2ee590a16f765ab6daccc5c12288b05e6
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-11-24 12:34:34 +0300

    feat: add buttons

--- content-script.js
+++ content-script.js
@@ -1,1 +1,26 @@
-console.log("init");
+function addButtons() {
+  const cards = document.querySelectorAll(".card");
+
+  [...cards].forEach((card) => {
+    const button = document.createElement("button");
+    button.textContent = "+";
+    button.addEventListener("click", () => {
+      const artist = card.querySelector(".col-xs-3 p").textContent;
+      const song = card.querySelector(".col-xs-4 p").textContent;
+      console.log(`${artist} - ${song}`);
+    });
+
+    card.querySelector(".pull-right").prepend(button);
+  });
+}
+addButtons();
+
+let observer = new MutationObserver((mutationList) => {
+  for (let record of mutationList) {
+    if (record.target.className !== "list-body") return;
+  }
+  console.log("mutation");
+  addButtons();
+});
+
+observer.observe(document, { childList: true, subtree: true });
